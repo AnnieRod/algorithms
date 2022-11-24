b5278c53e6ec0914bc30e4aa1f05fb59a43e0fbc
AnnieRod <euphie.13@gmail.com>
2022-11-23 20:02:39 -0500

    second

--- fund_todo4.js
+++ fund_todo4.js
@@ -6,4 +6,12 @@ function deleteNone(arr,x) {
 
 deleteNone([2,4,6,8,10], 3);
 
+//Devuelve la intersección en x de Y = MX + B
+function intercept(m, b) {
+    return m[0] - m[1]*(b[0]-m[0])/(b[1]-m[1]);
+}
+
+var test = intercept([5, 3], [3, 4]); 
+console.log(test)
+
 //
