47b823bb05b6fd6442ba387ea349df775aa4bdbf
AnnieRod <euphie.13@gmail.com>
2022-11-28 19:34:49 -0500

    stars and stuff

--- fundii_todo1.js
+++ fundii_todo1.js
@@ -22,3 +22,23 @@ function factorial(num) {
 }
 
 factorial(5);
+
+//'Arte de estrellas': alinea a un lado u otro segun la funcion
+function drawLeftStars(num, char) {
+    console.log(char.repeat(num) + ' '.repeat(75 - num));
+}
+
+drawLeftStars(2, "*");
+
+function drawRightStars(num, char) {
+    console.log(' '.repeat(75 - num) + char.repeat(num));
+}
+
+drawRightStars(2, "*");
+
+function drawCenterStars(num, char) {
+    var middle = (75 - num) / 2
+    console.log(' '.repeat(middle) + char.repeat(num) + ' '.repeat(middle));
+}
+
+drawCenterStars(2, "*");
