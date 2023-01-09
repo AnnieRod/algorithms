6c74d56a06484490c029479a334945ee6c6baca3
AnnieRod <euphie.13@gmail.com>
2023-01-09 15:24:08 -0500

    solved exercise

--- fundii_todo3.js
+++ fundii_todo3.js
@@ -1,1 +1,25 @@
-//Camisetas: Si compras 1 tienes 9% de descuento, si 2 19% y si 3 then 35%. Redondea al valor mas cercano 
+//Camisetas: Si compras 2 tienes 9% de descuento, si 3 19% y si 4 o más then 35%. Redondea al valor mas cercano 
+
+function sweatshirtPrice(num) {
+    var total = num * 20;
+    if (num == 2) {
+        total*= 0.91;
+    }
+    if (num == 3) {
+        total *= 0.81;
+    }
+    if (num >=4) {
+        total *= 0.65;
+    }
+
+    var price = Math.ceil(total);
+    return price 
+}
+
+console.log(sweatshirtPrice(1));
+console.log(sweatshirtPrice(2));
+console.log(sweatshirtPrice(3));
+console.log(sweatshirtPrice(4));
+console.log(sweatshirtPrice(5));
+
+//
