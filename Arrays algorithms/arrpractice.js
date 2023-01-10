110b0328e47e4a14d5ef0546c19070d172b26de8
AnnieRod <euphie.13@gmail.com>
2023-01-10 11:25:54 -0500

    new exercise on arrays

---
+++ Arrays algorithms/arrpractice.js
@@ -0,0 +1,12 @@
+arraySample = [3,6,67,6,23,11,100,8,93,0,17,24,7,2,33,45,28,33,23,12,99,100];
+
+//Hallar el numero maximo en un arreglo
+
+
+//Hallar el numero minimo en un arreglo
+
+
+//Hallar el promedio de los valores
+
+
+//Hallar la suma de los valores
