61fc7b1ebbea81260e30251d4c70a129d6f2987c
AnnieRod <euphie.13@gmail.com>
2023-01-10 11:35:50 -0500

    min and max done

--- Arrays algorithms/arrpractice.js
+++ Arrays algorithms/arrpractice.js
@@ -1,12 +1,40 @@
-arraySample = [3,6,67,6,23,11,100,8,93,0,17,24,7,2,33,45,28,33,23,12,99,100];
+arraySample = [3,6,67,6,23,11,100,8,93,2,17,24,7,2,33,45,28,120,33,23,12,99,100];
 
 //Hallar el numero maximo en un arreglo
 
+function arrMax(arr) {
+    var maxValue = arr[0];
+    for (var i = 0; i<arr.length; i++) {
+        if(maxValue < arr[i]) {
+            maxValue = arr[i];
+        }
+    }
+    console.log(maxValue);
+}
+
+arrMax(arraySample);
 
 //Hallar el numero minimo en un arreglo
+function arrMin(arr) {
+    var minValue = arr[0];
+    for (var i = 0; i<arr.length; i++) {
+        if(arr[i] < minValue) {
+            minValue = arr[i];
+        }
+    }
+    console.log(minValue);
+}
 
-
-//Hallar el promedio de los valores
-
+arrMin(arraySample);
 
 //Hallar la suma de los valores
+
+function arrSum(arr){
+    
+}
+
+//Hallar el promedio de los valores
+function arrMean(arr) {
+
+}
+
