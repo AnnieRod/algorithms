abe692bef1ea9e0c50bfb8b92b0bff1354ba62fc
AnnieRod <euphie.13@gmail.com>
2023-01-12 13:40:38 -0500

    all solved by arrays

--- Arrays algorithms/arrpractice.js
+++ Arrays algorithms/arrpractice.js
@@ -109,5 +109,28 @@ function popIndex(arr, index) {
 
 popIndex(array2, 5);
 
+// Swap positions of successive pairs of values of given array. If length is odd, do not change the final element. For [1,2,3,4], return [2,1,4,3]. For example, change input ["Brendan",true,42] to [true,"Brendan",42]. 
+// As with all array challenges, do this without using any built-in array methods.
+function swap(arr) {
+	for(let i = 0; i < arr.length - 1; i = i + 2) {
+		let temp = arr[i];
+		arr[i] = arr[i + 1];
+		arr[i + 1] = temp;
+	}
+}
+
+swap(array2);
+
+// Given a sorted array, remove duplicate values. Because array elements are already in order, all duplicate values will be grouped together. 
+function removeDupes(arr) {
+	let newArr = [];
+	for(let i = 0; i < arr.length; i++) {
+		if(arr[i] !== arr[i+1]) 
+			newArr.push(arr[i])
+	}
+	return newArr;
+}
+
+removeDupes(array2);
 
 
