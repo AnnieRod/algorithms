9ba7932756ba3bd75e7d67f1e819543bd82f9874
AnnieRod <euphie.13@gmail.com>
2023-01-13 15:54:08 -0500

    kinda understanding lists

--- linked lists/SLL_Todo1.js
+++ linked lists/SLL_Todo1.js
@@ -1,11 +1,25 @@
-
-var head; //cabeza de la lista
-
 //Clase del nodo 
+
 class Node {
     constructor(value) {
         this.data = value;
         this.next = null;
     }
 }
+
+//Crea nuevo nodo para encabezar la lista y devolver un puntero al nuevo nodo principal
+
+class SLL {
+    constructor() {
+        this.head = null;
+    }
+    addFront(value) {
+        var nodeNew = new Node(value);
+        nodeNew.next = this.head;
+        this.head = nodeNew;
+        return this.head;
+    }
+}
+
+
 
