e0f154d638c222785333d627205e3381d6f195f7
AnnieRod <euphie.13@gmail.com>
2023-01-14 13:52:34 -0500

    in the middle of problems

--- queues and stacks/stack_basics.js
+++ queues and stacks/stack_basics.js
@@ -1,3 +1,40 @@
 
 //STACK IMPLEMENTATON BASED ON ARRAY (Different than the usual methods)
 
+function arrayStack(){
+    var arr = [];
+
+
+//add values to the stack (push)
+    
+    this.push = function(value) {
+        arr[arr.length] = value;
+        return this;
+    }
+
+//return WITHOUT REMOVING  the top value of the stack 
+    
+    this.top = function() {
+        tophead = arr[arr.length-1];
+        return tophead;
+    }
+
+//return if the stack is empty
+
+    this.isEmpty = function() {
+        if (arr.length == 0) {
+            return true;
+        }
+        else {
+            return false
+        }
+    }
+
+
+//remove and return the top value (pop)
+
+//check if the stack contains some value
+
+//return the number of stacked values
+
+}
