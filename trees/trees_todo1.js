a34c1a0ded6f2526d264a9a6b5f34846c0281b1b
AnnieRod <euphie.13@gmail.com>
2023-01-13 21:14:29 -0500

    all solved about BST, test pending

--- trees/trees_todo1.js
+++ trees/trees_todo1.js
@@ -65,6 +65,56 @@ class treeBST {
         return false;
     }
 
-//
+//Devuelve el valor más pequeñeo encontrado en el arboll
+
+    min() {
+        var runner = this.root;
+        var min = this.root.value;
+        while(runner.left) {
+            if(runner.left.value < min) {
+                min = runner.left.value;
+            }
+            runner = runner.left;
+        }
+        return min;
+    }
+
+//Devuelve el valor más grande encontrado en el arbol
     
+    max() {
+        var runner = this.root;
+        var max = this.root.value;
+        while(runner.right) {
+            if(runner.right.value > max) {
+                max = runner.right.value;
+            }
+            runner = runner.right;
+        }
+        return max;
+    }
+
+//Retorna el numero de nodos (o valores) contenidos en el arbol
+
+    size() {
+        if(this.root === null) {
+            return 0;
+        }
+        function sizeNumber(runner) {
+            if(!runner) {
+                return 0;
+            }
+            return 1 + sizeNumber(runner.left) + sizeNumber(runner.right);
+        }
+        return sizeNumber(this.root);
+    }
+
+//Retorna si e arbol esta vacio, es decir, no contiene ningun valor
+
+    treeEmpty() {
+        if(this.root) {
+            return false;
+        }
+        return true;
+    }
+
 }
