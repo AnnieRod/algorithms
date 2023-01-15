1439fb3e55a2721826772d7b9e4ab1d013817823
AnnieRod <euphie.13@gmail.com>
2023-01-15 13:48:58 -0500

    starting to learn how to sort algorithms

---
+++ sort/sort_basics.py
@@ -0,0 +1,10 @@
+
+# create a function that uses BubbleSort to sort an array
+
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
