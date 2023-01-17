66b874a1bab9484086af11757316d0a0c263cc78
AnnieRod <euphie.13@gmail.com>
2023-01-16 19:19:25 -0500

    nailed it

--- linked lists/doublelinked_todo1.js
+++ linked lists/doublelinked_todo1.js
@@ -51,3 +51,30 @@ class SList {
     }   
 
 }
+
+//TESTING OF THE WHOLE METHOD of K's
+
+let list = new SList();
+
+let node1 = new Node(1);
+let node2 = new Node(2);
+let node3 = new Node(3);
+let node4 = new Node(4);
+let node5 = new Node(5);
+
+node1.next = node2;
+node2.prev = node1;
+node2.next = node3;
+node3.prev = node2;
+node3.next = node4;
+node4.prev = node3;
+node4.next = node5;
+node5.prev = node4;
+
+list.head = node1;
+
+console.log(list.findNode(1)); // 5
+console.log(list.findNode(2)); // 4
+console.log(list.findNode(3)); // 3
+console.log(list.findNode(4)); // 2
+console.log(list.findNode(5)); // 1
