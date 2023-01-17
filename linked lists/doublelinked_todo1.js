37bf55422620d899c02754be53d4dbffaf9f74b4
AnnieRod <euphie.13@gmail.com>
2023-01-16 19:16:33 -0500

    solved and ready to try

--- linked lists/doublelinked_todo1.js
+++ linked lists/doublelinked_todo1.js
@@ -27,4 +27,27 @@ class SList {
         }
         this.head = prevNode;
     }
+
+//Dado K, retorna el valor  para el nodo k contando desde el final de la lista
+
+    findNode(k) {
+        let  runner = this.head;
+        let count = 0;
+
+        while(runner) {
+            runner = runner.next;
+            count++;
+        }
+
+        if (k > count) {
+            return null;
+        }
+
+        runner = this.head;
+        for (var i = 1; i < count - k +1; i++) {
+            runner = runner.next;
+        }
+        return runner.data
+    }   
+
 }
