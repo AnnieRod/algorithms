ef30431830ba81be68c82d4a08bb60bd6abbbeb0
AnnieRod <euphie.13@gmail.com>
2023-01-16 19:07:56 -0500

    understanding reversion in linked list

--- linked lists/doublelinked_todo1.js
+++ linked lists/doublelinked_todo1.js
@@ -1,1 +1,30 @@
-//reverse a node sequence
+//reverse a node sequence in a double linked list
+
+//create the node
+class Node {
+    constructor(data, prev = null, next = null) {
+        this.data = data;
+        this.prev = prev;
+        this.next = next;
+    }
+}
+
+//create the list with its methods to "point and reverse"
+class SList {
+    constructor() {
+        this.head = null;
+    }
+
+    reverse() {
+        let runner = this.head;
+        let prevNode = null;
+        while (runner) {
+            let nextNode = runner.next;
+            runner.next = prevNode;
+            runner.prev = nextNode;
+            prevNode = runner;
+            runner = nextNode;
+        }
+        this.head = prevNode;
+    }
+}
