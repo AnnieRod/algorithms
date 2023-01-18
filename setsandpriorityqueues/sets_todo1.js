f8244301585f74454ca2654d5e4189c45d3f50ec
AnnieRod <euphie.13@gmail.com>
2023-01-17 19:20:16 -0500

    already orderde one

--- setsandpriorityqueues/sets_todo1.js
+++ setsandpriorityqueues/sets_todo1.js
@@ -21,4 +21,37 @@ function interleaveArrays(arr1, arr2) {
     return newArr;
 }
 
-console.log(interleaveArrays([77,22,11,22], [2,6,7,2,6,2]));
+interleaveArrays([77,22,11,22], [2,6,7,2,6,2]);
+
+// Merge two sorted arrays into a new one with a multiset of all values
+
+function mergeArrays(arr1, arr2) {
+    let newArr = [];
+    let i = 0;
+    let j = 0;
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            newArr.push(arr1[i]);
+            i++;
+        } else if (arr1[i] > arr2[j]) {
+            newArr.push(arr2[j]);
+            j++;
+        } else {
+            newArr.push(arr1[i]);
+            newArr.push(arr2[j]);
+            i++;
+            j++;
+        }
+    }
+    while (i < arr1.length) {
+        newArr.push(arr1[i]);
+        i++;
+    }
+    while (j < arr2.length) {
+        newArr.push(arr2[j]);
+        j++;
+    }
+    return newArr;
+}
+
+mergeArrays([1,2,2,2,7], [2,2,6,6,7]);
